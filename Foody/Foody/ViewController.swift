e76812990c738b9fd5c18a32613c2b561ac5021b
Bhavik Kothari <bhavikkothari@Bhaviks-MacBook-Pro.local>
2020-06-05 21:11:49 -0400

    Add Camera Icon

---
+++ Foody/Foody/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  Foody
+//
+//  Created by Bhavik Kothari on 2020-06-05.
+//  Copyright © 2020 Bhavik Kothari. All rights reserved.
+//
+
+import UIKit
+import CoreML
+import Vision
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let imagePicker = UIImagePickerController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = false
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let userPickedimage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+        imageView.image = userPickedimage
+        }
+        
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+
+    
+    @IBAction func cameraPressed(_ sender: Any) {
+         present(imagePicker, animated: true, completion: nil)
+    }
+    
+}
+
